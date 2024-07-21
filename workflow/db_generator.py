0eff4117f3d6549c2e8781eefa15872f25e7603e
Jorge González García <jorgegonzalezvet@gmail.com>
2024-07-21 23:06:33 +0100

    Generated ltr_retriever.py; Added some prototypes of LTRHarvest functions to db_utils.py; Now scientific name retrieval from tax_id is performed only once and stored in a dictionary for efficiency.

--- workflow/db_generator.py
+++ workflow/db_generator.py
@@ -3,8 +3,11 @@ This script generates the BLAST databases from a collection of (in principle) ta
 FASTA files in the specified directories. The databases are generated in their corresponding subdirectories,
 the defaults lists are updated with the names of the databases, and the directories are checked for existing
 databases to avoid re-generating them.
+This script also generates the Suffixerator index, and uses it to generate the LTRHarvest output.
 
 CAUTION: Misnamed files in the input directory will result in duplicated databases.
+CAUTION 2: This script relies on the previous generation of the BLAST databases within their directories.
+
 """
 import os
 
@@ -32,9 +35,13 @@ if __name__ == '__main__':
     species_files = utils.directory_file_retriever(defaults.SPECIES_DB)
     virus_files = utils.directory_file_retriever(defaults.VIRUS_DB)
 
+    # Generate a dictionary with the taxid: name pairs
+    species_dict: dict = {file.split('.')[0]: db_utils.get_species_name_from_file(file) for file in species_files}
+    virus_dict: dict = {file.split('.')[0]: db_utils.get_species_name_from_file(file) for file in virus_files}
+
     # Populate the species and virus lists in defaults
-    defaults.SPECIES = list(set(db_utils.taxlist2name(os.listdir(defaults.SPECIES_DB))))
-    defaults.VIRUS = list(set(db_utils.taxlist2name(os.listdir(defaults.VIRUS_DB))))
+    defaults.SPECIES = list(set(species_dict.values()))
+    defaults.VIRUS = list(set(virus_dict.values()))
 
     # Clean the file lists if directory with database already exists
     species_files = db_utils.existing_db_list_cleaner(species_files, defaults.SPECIES_DB)
@@ -43,8 +50,13 @@ if __name__ == '__main__':
     # Generate the databases
     db_utils.directory_db_generator(file_list=species_files,
                                     input_db=defaults.SPECIES_DB,
-                                    db_type='nucl')
+                                    db_type='nucl',
+                                    tax2sc_dict=species_dict)
 
     db_utils.directory_db_generator(file_list=virus_files,
                                     input_db=defaults.VIRUS_DB,
-                                    db_type='nucl')
+                                    db_type='nucl',
+                                    tax2sc_dict=virus_dict)
+
+
+
