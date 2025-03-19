f8724927bf354ae08a6fdb63d19dfb0cbfd61eac
Jorge Gonzalez <jorgegonzalezvet@gmail.com>
2025-03-20 00:27:10 +0100

    MAJOR: Displaced all major configuration to config.yaml; Removed NCBI API token as plain text input, now it must be set as an environment variable; Removed everything with concurrent.futures, now everything that has to be parallelized is done so by Snakemake; Removed rate_limiter function in utils.py, as it won't be necessary for so few sequences now

--- workflow/defaults.py
+++ workflow/defaults.py
@@ -1,78 +1,61 @@
+import yaml
 import os
+
+CONFIG_FILE = os.path.abspath(os.path.join('..', 'data', 'config', 'config.yaml'))
+with open(CONFIG_FILE, 'r') as f:
+    config = yaml.safe_load(f)
 
 # BLAST
 EXPANSION_SIZE: int = 0
-E_VALUE: float = 0.09
-ACCESSION_ID_REGEX: str = r'[A-Z]{2,}_?[0-9]+\.[0-9]{1,2}'
-PROBE_MIN_LENGTH: dict = {
-    'GAG': 200,
-    'POL': 400,
-    'ENV': 200,
-    'VIF': 200,
-    'N_protein': 200,
-    'P_protein': 200,
-    'G_protein': 200,
-    'L_protein': 200,
-    'X_protein': 200,
-    'M_protein': 200,
-    'Pr160': 200
-}
+E_VALUE = config['blast']['e_value']
+ACCESSION_ID_REGEX = config['blast']['accession_id_regex']
+PROBE_MIN_LENGTH = config['blast']['probe_min_length']
 
 # Logging
-LEVEL_STYLES: dict = {
-    'debug': {'color': 'white'},                    # Standard debug level
-    'info': {'color': 'cyan', 'bold': 'yes'},       # Standard info level
-    'warning': {'color': 'yellow'},                 # Standard warning level
-    'error': {'color': 'red', 'bold': 'yes'},       # Standard error level
-    'critical': {'color': 'black', 'bold': 'yes', 'background': 'red'},  # Standard critical level
-}
-
-FIELD_STYLES: dict = {
-    'asctime': {'color': 'green'},
-    'hostname': {'color': 'green'},
-    'levelname': {'color': 'green'},
-    'name': {'color': 'green'},
-    'programname': {'color': 'green'},
-    'username': {'color': 'green'},
-    'process': {'color': 'green'},
-    'thread': {'color': 'green'}
-}
+LEVEL_STYLES = config['logging']['level_styles']
+FIELD_STYLES = config['logging']['field_styles']
 
 # Databases
-ROOT_CONFIG_FILE = os.path.abspath(os.path.join('..', 'data', 'config', 'root_folder.txt'))
-with open(ROOT_CONFIG_FILE, 'r') as f:
-    ROOT = os.path.abspath(f.readline().strip())
-
+ROOT = os.path.abspath(config['root'].get('db_root_folder', os.path.join('..')))
 ROOT_DB = os.path.abspath(os.path.join(ROOT, 'local'))
 SPECIES_DB = os.path.abspath(os.path.join(ROOT_DB, 'blast_dbs', 'species'))
 ACCESSORY_DB = os.path.abspath(os.path.join(ROOT, 'accessory'))
 
 # Directories
-DATA_DIR = os.path.abspath(os.path.join('..', 'data'))
-RESULTS_DIR = os.path.abspath(os.path.join('..', 'results'))
-LOG_DIR = os.path.abspath(os.path.join('..', 'logs'))
-WORKFLOW_DIR = os.path.abspath(os.path.join('..', 'workflow'))
+DATA_DIR = os.path.abspath(os.path.join(config['root'].get('data_root_folder', os.path.join(ROOT, 'data'))))
+RESULTS_DIR = os.path.abspath(os.path.join(config['root'].get('results_root_folder', os.path.join(ROOT, 'results'))))
+LOG_DIR = os.path.abspath(os.path.join(config['root'].get('log_root_folder', os.path.join(ROOT, 'logs'))))
+WORKFLOW_DIR = os.path.abspath(os.path.join(config['root'].get('workflow_root_folder', os.path.join(ROOT, 'workflow'))))
+
+DOWNLOAD_LOG = os.path.abspath(os.path.join(LOG_DIR, 'download_log.log'))
+
+HMM_PROFILE_DIR = os.path.abspath(os.path.join(ACCESSORY_DB, 'hmm_profiles'))
+
 SPECIES_DIR = os.path.abspath(os.path.join(DATA_DIR, 'species'))
 TABLE_INPUT_DIR = os.path.abspath(os.path.join(DATA_DIR, 'tables'))
 PICKLE_DIR = os.path.abspath(os.path.join(DATA_DIR, 'pickles'))
-TBLASTN_PICKLE_DIR = os.path.abspath(os.path.join(PICKLE_DIR, 'tblastn'))
 TMP_DIR = os.path.abspath(os.path.join(DATA_DIR, 'tmp'))
+
+TBLASTN_PICKLE_DIR = os.path.abspath(os.path.join(PICKLE_DIR, 'tblastn'))
+
 TABLE_OUTPUT_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'tables'))
 PLOT_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'plots'))
+TRACK_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'tracks'))
+LTRHARVEST_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'ltrharvest'))
+LTRDIGEST_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'ltrdigest'))
+
 CIRCLE_PLOT_DIR = os.path.abspath(os.path.join(PLOT_DIR, 'circle_plots'))
 HOTSPOT_PDF_DIR = os.path.abspath(os.path.join(PLOT_DIR, 'hotspot_pdfs'))
-TRACK_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'tracks'))
+
 TRACK_ORIGINAL_DIR = os.path.abspath(os.path.join(TRACK_DIR, 'original'))
 TRACK_CANDIDATES_DIR = os.path.abspath(os.path.join(TRACK_DIR, 'candidates'))
 TRACK_VALIDATED_DIR = os.path.abspath(os.path.join(TRACK_DIR, 'validated'))
 TRACK_HOTSPOTS_DIR = os.path.abspath(os.path.join(TRACK_DIR, 'hotspots'))
+
 TABLE_HOTSPOT_DIR = os.path.abspath(os.path.join(TABLE_OUTPUT_DIR, 'hotspots'))
-LTRHARVEST_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'ltrharvest'))
-LTRDIGEST_DIR = os.path.abspath(os.path.join(RESULTS_DIR, 'ltrdigest'))
 TABLE_OVERLAP_MATRIX_DIR = os.path.abspath(os.path.join(TABLE_OUTPUT_DIR, 'overlap_matrix'))
 SEGMENTED_SPECIES_DIR = os.path.abspath(os.path.join(TABLE_OUTPUT_DIR, 'segmented_species'))
 PLOT_DATAFRAMES_DIR = os.path.abspath(os.path.join(TABLE_OUTPUT_DIR, 'plot_dataframes'))
-HMM_PROFILE_DIR = os.path.abspath(os.path.join(ROOT, 'accessory', 'hmm_profiles'))
 
 # Directory generation
 os.makedirs(ROOT, exist_ok=True)
@@ -106,38 +89,23 @@ os.makedirs(PLOT_DATAFRAMES_DIR, exist_ok=True)
 os.makedirs(HMM_PROFILE_DIR, exist_ok=True)
 
 # Execution and requests
-USE_SPECIES_LIST: bool = False
-MAX_RETRIEVAL_ATTEMPTS: int = 3
-MAX_EXECUTION_ATTEMPTS_PER_SECOND: int = 10
-MIN_EXECUTION_INTERVAL: int = 1  # seconds
-MAX_THREADPOOL_WORKERS: int = None  # In my laptop, 7 is the maximum
-GENBANK_RETRIEVAL: bool = False
-ENTREZ_EMAIL: str = 'jgonzlez@tcd.ie'
-NCBI_API_TOKEN: str = 'faa9e17bb461e82963f079c167ec5c7aac08'
+USE_SPECIES_LIST = config['execution']['use_species_list']
+MAX_RETRIEVAL_ATTEMPTS = config['execution']['max_retrieval_attempts']
+MAX_THREADPOOL_WORKERS = config['execution']['max_threadpool_workers']
+ENTREZ_EMAIL = config['execution']['entrez_email']
 
-# Displays
-DISPLAY_REQUESTS_WARNING: bool = False
-DISPLAY_OPERATION_INFO: bool = False
+# Display
+DISPLAY_REQUESTS_WARNING: bool = config['display']['display_requests_warning']
+DISPLAY_OPERATION_INFO: bool = config['display']['display_operation_info']
 
 # CSV
-CSV_DELIMITER: str = ','
-CSV_ATTRIBUTES: list[str] = [
-    'Family',
-    'Virus',
-    'Abbreviation',
-    'Species',
-    'Probe',
-    'Accession',
-    'Identifier',
-    'Strand'
-]
+CSV_DELIMITER: str = config['tables']['delimiter']
+CSV_ATTRIBUTES: list[str] = config['tables']['attributes']
 
 # Genomes
-SPECIES_FILE = os.path.abspath(os.path.join(SPECIES_DIR, 'species.txt'))
+SPECIES_FILE: str = config['input']['species_file']
 
 if not USE_SPECIES_LIST:
     SPECIES: list = [(f.split('.fa')[0]).strip() for f in os.listdir(SPECIES_DB) if f.endswith('.fa')]
 else:
     SPECIES: list = [line.strip() for line in open(SPECIES_FILE, 'r')]
-
-VIRUS: list = ['NCBI_Virus']
